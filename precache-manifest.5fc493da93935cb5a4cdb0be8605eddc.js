e665f0a0ff26f80aa64d4e341915af8609f3267e
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-12-20 00:13:14 -0500

    Updates

---
+++ precache-manifest.5fc493da93935cb5a4cdb0be8605eddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d21a53217226625e6bb3a0f1bbd201c",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "64a591f9fb13668e7990",
+    "url": "/portfolio-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "961e21bbff7f8d40f559",
+    "url": "/portfolio-react/static/js/2.e87f306a.chunk.js"
+  },
+  {
+    "revision": "288f8795c77f6dd607dd5879eebc8c76",
+    "url": "/portfolio-react/static/js/2.e87f306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610ec27c5c68d9bbdf29",
+    "url": "/portfolio-react/static/js/3.863640bc.chunk.js"
+  },
+  {
+    "revision": "64a591f9fb13668e7990",
+    "url": "/portfolio-react/static/js/main.c52665fc.chunk.js"
+  },
+  {
+    "revision": "1615c8c1a313e9ffd6c2",
+    "url": "/portfolio-react/static/js/runtime-main.78a5cb74.js"
+  }
+]);
